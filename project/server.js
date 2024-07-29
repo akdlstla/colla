b5e9049dc32690f7873ace01ff7ec04be432e981
gogumastick <jarloperak@hotmail.com>
2024-07-29 16:53:58 +0900

    07291653

--- project/server.js
+++ project/server.js
@@ -3,7 +3,14 @@ const db = require('./models');
 const app = express();
 const PORT = 8000;
 
-
+app.set("view engine", "ejs");
+app.use(express.json());
+const pageRouter = require("./routes/page");
+app.use("/", pageRouter);
+// 404
+app.use("*", (req, res) => {
+  res.status(404).send("페이지를 찾을 수 없습니다.");
+});
 
 
 
