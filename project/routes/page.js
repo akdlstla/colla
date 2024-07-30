5fecaacabf02809cf8ac487a0e857349b19adeb4
akdlstla <akdlstla@naver.com>
2024-07-30 18:11:07 +0900

    07301811

--- project/routes/page.js
+++ project/routes/page.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { main,signup } = require("../controller/page");
+const { main,signup,terms,personal } = require("../controller/page");
 const router = express.Router();
 
 
 router.get("/", main);
 router.get('/signup',signup)
+router.get('/terms',terms)
+router.get('/personal',personal)
 
 
 module.exports = router;
