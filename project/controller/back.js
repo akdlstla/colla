8bcf332e06893249426924fce1b52d5efada2c84
jiheechai <jjihee97@naver.com>
2024-08-02 19:38:25 +0900

    2024.08.02.19:38 chai

--- project/controller/back.js
+++ project/controller/back.js
@@ -19,8 +19,8 @@ const signup = async (req, res) => {
             
             const bcryppass = await bcrypt.hash(password, salt);
             const result = await user.create({ username, email, password:bcryppass, department });
-            console.log('signup 결과값:', result);
-            res.json({ result: true, message: '가입 완료' });
+            console.log('signup', result);
+           
         }
     } catch (error) {
         console.log(error);
@@ -28,23 +28,12 @@ const signup = async (req, res) => {
     }
 };
 const login = async (req, res) => {
-    // if (!email || !password) {
-    //     return res.status(400).json({ message: '이메일과 비밀번호를 입력하세요.' });
-    // };
-
     try {
-        const { email, password, rememberMe } = req.body;
-        console.log( 'login 값 :', email, password , rememberMe);
-
-        // 사용자 확인
-        // const findAll = await user.findAll()
-        // console.log(findAll)
-
+        const { email, password } = req.body;
         const find = await user.findOne({ where: { email } });
-        console.log('find 값 :', find);
         if (find) {
             const bcryppass = await bcrypt.compare(password, find.password);
-            console.log('bcryppass 값 : ', bcryppass);
+            console.log(bcryppass);
             if (bcryppass) {
                 //jwt토큰 발생
                 /**
@@ -53,30 +42,17 @@ const login = async (req, res) => {
                  * issuer: 토큰발급자 지정
                  */
                 const token = jwt.sign({ id: find.id, email: find.email }, process.env.LOGSECRET, { expiresIn: '24h' });
-                console.log('토큰값 :', token);
-                console.log('환경변수LOGSECRET값:', process.env.LOGSECRET);
+                console.log(process.env.LOGSECRET);
                 const response = {
                     token,
                 };
-                res.json({ result: true, response, message: '로그인 완료' });
-
-                // 쿠키
-            
-                // const expiryDate = new Date();
-                // expiryDate.setDate(expiryDate.getDate() + 7);
-                // res.cookie('email', response , { expires: expiryDate, httpOnly: true});
-                // console.log('쿠키생성까지 읽은건데');
-                
-                // const email = req.cookies.response;
-                // res.render('login', { email: response });
-                
-                
+                res.json({ result: true, code: 100, response, message: '로그인 완료' });
             } else {
-                res.json({ result: false, message: '비밀번호 틀렸네?.' });
+                res.json({ result: false, code: 1000, response: null, message: '비밀번호 틀렸네?.' });
             }
         } else {
-            res.json({ result: false, message: '회원이 아닌데?.' });
-        };
+            res.json({ result: false, code: 1001, response: null, message: '회원이 아닌데?.' });
+        }
     } catch (error) {
         console.log(error);
         res.status(500).json({ result: false, message: '서버오류' });
--- project/controller/page.js
+++ project/controller/page.js
@@ -13,24 +13,5 @@ const personal = (req,res) =>{
     res.render('personal')
 }
 
-const chat = (req,res) =>{
-  res.render("chat")
-}
-
-const searchResult = (req,res) =>{
-  res.render("searchResult")
-}
-
-const noSearchResult = (req,res) =>{
-  res.render("noSearchResult")
-}
-const login = (req,res) =>{
-  res.render("login")
-}
-const search = (req,res) =>{
-  res.render('search')
-}
-
-module.exports = { main, signup, chat, searchResult, noSearchResult, terms, personal, login, search };
-
+module.exports = { main, signup, terms, personal };
 
