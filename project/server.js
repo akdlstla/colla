784f213e350aa2484f439698e4999fbf14f5a60e
jiheechai <jjihee97@naver.com>
2024-08-05 20:41:56 +0900

    2024.08.05.20:41 chai

--- project/server.js
+++ project/server.js
@@ -5,13 +5,12 @@ const PORT = 8000;
 // const aws = require('aws-sdk')
 // const multer = require('multer')
 // const multerS3 = require('multer-s3')
-
+const socketIo = require('socket.io');
+const http = require('http');
+const server = http.createServer(app);
+const io = socketIo(server);
 app.set('view engine', 'ejs');
 app.set('views', './views');
-// const socketIo = require('socket.io');
-// const http = require('http');
-// const server = http.createServer(app);
-// const io = socketIo(server);
 
 //aws 설정
 // aws.config.update({
@@ -44,44 +43,49 @@ const backRouter = require('./routes/back')
 app.use('/api/colla', backRouter)
 
 
-// /** 1. 소켓 연결 */
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
+/** 1. 소켓 연결 */
+io.on('connection', (socket) => {
+  console.log('a user connected');
 
-// //   /** 1. 내 소켓 아이디 저장.*/
-//   socket.on('login', );
+//   /** 1. 내 소켓 아이디 저장.*/
+  // socket.on('login', );
 
-// //   /** 2. 방 입장 */
-//   socket.on('join chat', (chat) => {
-//     //join : 방 없으면 생성, 있으면 입장
-//       socket.join(chat);
-//       socket.chat = chat;
-//       console.log(`User joined room: ${room}`);
-//   });
+//   /** 2. 방 입장 */
+  socket.on('join chat', (chat) => {
+    //join : 방 없으면 생성, 있으면 입장
+      socket.join(chat);
+      socket.chat = chat;
+      console.log(chat);
+      
+      console.log(`User joined room: ${chat}`);
+  });
 
 
-// //   /** 3. 룸 내 메세지 브로드캐스트*/
-//   socket.on('chat message', ({myname, value}) => {
-//     io.to(socket.chat).emit('chat message', {myname, value});
-//   });
+//   /** 3. 룸 내 메세지 브로드캐스트*/
+  socket.on('chat message', (arg) => {
+    const {username, value} = arg;
+    console.log("server: ", username, value);
+    
+    io.to(socket.chat).emit('chat message', {username, value});
+  });
 
   
-// //   /** 4. socket 방 초대 */
-//   socket.on('invite chat', ({username, chat}) => {
-//     usersocket = username.socket.id;
-//     if(usersocket) {
-//       usersocket.join(chat);
-//       usersocket.emit('invited', chat);
-//       console.log(`User ${username} invited to chat ${chat}`);
-//     }
-//   });
+//   /** 4. socket 방 초대 */
+  socket.on('invite chat', (username, chat) => {
+    usersocket = username.socket.id;
+    if(usersocket) {
+      usersocket.join(chat);
+      usersocket.emit('invited', chat);
+      console.log(`User ${username} invited to chat ${chat}`);
+    }
+  });
 
 // //   /** 5. 로그아웃 : 소켓 연결 해제 */
 //   socket.on('disconnect', () => {
 //     console.log('user disconnected');
 //     })
 //   });
-
+})
 
 // 404
 app.use("*", (req, res) => {
@@ -89,7 +93,7 @@ app.use("*", (req, res) => {
 });
 
 db.sequelize.sync({ force: false}).then(() => {
-    app.listen(PORT,() => {
+    server.listen(PORT,() => {
         console.log(`http://localhost:${PORT}`);
     })
 })
